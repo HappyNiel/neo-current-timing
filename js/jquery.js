f6787a0bee657149cac687b4ee39c77f5b9e75ea
Niel Hekkens <nielhekkens@gmail.com>
2020-02-01 21:28:46 +0100

    Adjust the penalty script to adapt to the 24h series way of serving penalties

--- js/jquery.js
+++ js/jquery.js
@@ -1,18 +1,18 @@
 // JavaScript Document
 $(document).ready(function(){
 	raceControlMessages();
-	
+
 	setInterval(function(){
 		raceControlMessages();
 	}, 120000);
 });
 
-var id_case = 0; 
+var id_case = 0;
 console.log('init id:' + id_case);
 
 function raceControlMessages(){
 	var url='https://www.neo-endurance.com/php/get_msg_google.php';
-	
+
 	$.ajax({
 		url: url,
 		dataType: "json",
@@ -20,33 +20,33 @@ function raceControlMessages(){
 		success: function(json){
 			//get last entry
 			var last = json.length - 1;
-			console.log('new id:' + id_case);			
-			
+			console.log('new id:' + id_case);
+
 			var id_case_new = parseInt(json[last].caseID); //convert caseID to an integer
 			var penalty = json[last].penalty;
 			var length = json[last].length;
 			var car = json[last].car;
 			var reason = json[last].reason;
-			
+
 			//setup the messages
 			var message_time = length + ' sec ' + penalty + ' for car ' + car + ' - ' + reason;
 			var message_notime = penalty + ' for car ' + car + ' - ' + reason;
 			var message;
-			
+
 			//select the correct message based on the type of penalty
-			if (penalty === "S&H"){
+			if (penalty === "TIME PENALTY"){
 				message = message_time;
 				//$('#msg').html(message_time);
 			}
-			else if (penalty === "S&G" || penalty === "Warning #1" || penalty === "Warning"){
+			else if (penalty === "WARNING"){
 				message = message_notime;
 				//$('#msg').html(message);
 			}
-			
+
 			if(id_case != id_case_new){
 				$('#msg').css('background','#FFA200');
 				$('#msg').html(message);
-				id_case = id_case_new; 
+				id_case = id_case_new;
 			}
 			else if (id_case === id_case_new){
 				$('#msg').css('background','none');
